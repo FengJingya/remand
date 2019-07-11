ce1e193383baed0a4cba5218704477e65493ce6e
FengJingya <591573439@qq.com>
2019-07-11 17:57:43 +0800

    edit

--- runoob1.js
+++ runoob1.js
@@ -1,13 +1,2 @@
 require("./style.css");
-//document.write(require("./runoob2.js"));
-/*var canvas = document.createElement('canvas');
-canvas.width = 500;
-canvas.height = 500;
-document.body.appendChild(canvas);*/
-let canvas,ctx
-canvas=document.getElementById('canvas');
-ctx=canvas.getContext('2d');//画笔
-canvas.width=632;
-canvas.height=472;
-
-export {canvas,ctx}
+document.write(require("./runoob2.js"));
